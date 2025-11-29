4c6e8dccd16178500d8fd0a20f6f55c45a68909b
eMregg2002 <ericgalvezgal@gmail.com>
2025-11-29 13:03:34 -0500

    Generación de la clase Cliente

---
+++ Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace ecspage
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Documento { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
