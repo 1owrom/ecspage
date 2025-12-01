033b97ee6bf8e8ec218f5d31e19ac010890def7a
JhonCE03 <jhoncuba988@gmail.com>
2025-11-30 20:23:04 -0500

    Commit 3: Agregado IdCliente al DTO para soportar edición

--- Application/Contracts/DTOs.cs
+++ Application/Contracts/DTOs.cs
@@ -33,6 +33,7 @@ namespace ecspage.Application.Contracts
 
     public class FacturaDTO
     {
+        //Se agrega el campo IdCliente al DTO para permitir edición de factura
         public int Id { get; set; }
         public int IdCliente { get; set; }
         public string Serie { get; set; } = "";
